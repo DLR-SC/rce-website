f0eaafc32a40ccae366f65b7fb6d6ba835e08494
Andreas Schreiber <andreas.schreiber@medando.de>
2015-08-18 00:25:43 +0200

    Google Analytics and DISQUS added.

--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "rcenvironment"
+GOOGLE_ANALYTICS = "UA-25164892-2"
