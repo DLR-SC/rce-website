edb5e1c5e8b51f3bd94bad1bd532d1f6f3986049
Jan Flink <jan.flink@dlr.de>
2018-05-25 08:35:52 +0200

    Added a link to the DLR´s privacy page.

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,8 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Simulation and Software Technology', 'http://www.dlr.de/sc'),
-         ('Imprint', 'https://www.dlr.de/dlr/en/desktopdefault.aspx/tabid-10067/149_read-17/'),)
+         ('Imprint', 'https://www.dlr.de/dlr/en/desktopdefault.aspx/tabid-10067/149_read-17/'),
+		 ('Privacy', 'https://www.dlr.de/dlr/en/desktopdefault.aspx/tabid-11049/1890_read-27253/'),)
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/RCEnvironment'),
